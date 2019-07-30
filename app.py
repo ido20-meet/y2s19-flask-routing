48f90ddd72415a005735c4f47bbd9cb3f9b8675a
myname21-meet <myname21@meet.mit.edu>
2019-07-30 17:12:03 +0300

    sure

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html") 
+	return render_template("home.html") 
 
-@app.route('/hello')
-def hello_route():
-	return render_template('Hello.html', n='Emily')
+@app.route('/hello/<string:name>')
+def hello_name_route(name):
+	return render_template('home.html', n=name)
 
+@app.route('/name/<int:student_id>')
+def display_student(student_id=1):
+	return render_template('student.html', id_number=student_id, Student=query_by_id(student_id))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
